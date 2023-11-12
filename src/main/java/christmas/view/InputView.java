003d110420f351c2a2fba17c72200ae6c29a78a6
hlmg <devhlmg@gmail.com>
2023-11-12 19:28:01 +0900

    feat(InputView): 방문 날짜 입력 기능 구현

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,16 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public int getVisitDate() {
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        String input = Console.readLine();
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+}
