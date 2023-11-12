a4f542f7395b8fb1b4039f96881e8b7476b0ed3d
hlmg <devhlmg@gmail.com>
2023-11-12 19:51:40 +0900

    feat(InputView): 메뉴 입력 기능 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,6 +1,11 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.dto.OrderItemRequestDto;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class InputView {
 
@@ -12,5 +17,22 @@ public class InputView {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         }
+    }
+
+    public List<OrderItemRequestDto> getOrderItems() {
+        List<OrderItemRequestDto> orderItemRequestDtos = new ArrayList<>();
+        System.out.println("주문하실 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        String input = Console.readLine();
+        Pattern pattern = Pattern.compile("^([가-힣a-zA-Z]+-[0-9]+,?)+$");
+        Matcher matcher = pattern.matcher(input);
+        if (!matcher.find()) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+        String[] split = input.split(",");
+        for (String s : split) {
+            String[] split1 = s.split("-");
+            orderItemRequestDtos.add(new OrderItemRequestDto(split1[0], Integer.parseInt(split1[1])));
+        }
+        return orderItemRequestDtos;
     }
 }
