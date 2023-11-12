844e5d0cedb9ef0fc96ed960a60793822ad48263
hlmg <devhlmg@gmail.com>
2023-11-12 20:09:13 +0900

    feat(OutputView): 이벤트 혜택 미리 보기 메세지 출력

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -5,4 +5,9 @@ public class OutputView {
     public void printWelcome() {
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
     }
+
+    public void printEventPreview(int dayOfMonth) {
+        String message = String.format("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!", dayOfMonth);
+        System.out.println(message);
+    }
 }
