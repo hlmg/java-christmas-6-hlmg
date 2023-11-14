41e0b720634626dfcd759ecb91ef766df68d97fb
hlmg <devhlmg@gmail.com>
2023-11-15 00:29:49 +0900

    refactor(Menu, Type): 메뉴 종류를 나타내는 enum 클래스 추가
    기존에는 디저트인지 메인인지 음료인지 여부를 Menu 값으로 계산을 했는데, 새로운 메뉴를 추가하고 isXXX 메서드에는 추가하지
    않으면 버그가 발생한다. 따라서 Type을 나타내는 enum을 필드로 가지게 해서 실수를 방지하고자 함

--- src/main/java/christmas/domain/Menu.java
+++ src/main/java/christmas/domain/Menu.java
@@ -4,25 +4,27 @@ import java.util.Arrays;
 import java.util.Optional;
 
 public enum Menu {
-    BUTTON_MUSHROOM_SOUP("양송이수프", 6000),
-    TAPAS("타파스", 5500),
-    CAESAR_SALAD("시저샐러드", 8000),
-    T_BONE_STEAK("티본스테이크", 55000),
-    BARBECUE_RIBS("바비큐립", 54000),
-    SEAFOOD_PASTA("해산물파스타", 35000),
-    CHRISTMAS_PASTA("크리스마스파스타", 25000),
-    CHOCOLATE_CAKE("초코케이크", 15000),
-    ICE_CREAM("아이스크림", 5000),
-    ZERO_COKE("제로콜라", 3000),
-    RED_WINE("레드와인", 60000),
-    CHAMPAGNE("샴페인", 25000);
+    BUTTON_MUSHROOM_SOUP("양송이수프", 6_000, Type.APPETIZER),
+    TAPAS("타파스", 5_500, Type.APPETIZER),
+    CAESAR_SALAD("시저샐러드", 8_000, Type.APPETIZER),
+    T_BONE_STEAK("티본스테이크", 55_000, Type.MAIN),
+    BARBECUE_RIBS("바비큐립", 54_000, Type.MAIN),
+    SEAFOOD_PASTA("해산물파스타", 35_000, Type.MAIN),
+    CHRISTMAS_PASTA("크리스마스파스타", 25_000, Type.MAIN),
+    CHOCOLATE_CAKE("초코케이크", 15_000, Type.DESSERT),
+    ICE_CREAM("아이스크림", 5_000, Type.DESSERT),
+    ZERO_COKE("제로콜라", 3_000, Type.BEVERAGE),
+    RED_WINE("레드와인", 60_000, Type.BEVERAGE),
+    CHAMPAGNE("샴페인", 25_000, Type.BEVERAGE);
 
     private final String name;
     private final int price;
+    private final Type type;
 
-    Menu(String name, int price) {
+    Menu(String name, int price, Type type) {
         this.name = name;
         this.price = price;
+        this.type = type;
     }
 
     public static Optional<Menu> from(String name) {
@@ -36,18 +38,22 @@ public enum Menu {
     }
 
     public boolean isDessert() {
-        return this == CHOCOLATE_CAKE || this == ICE_CREAM;
+        return this.type == Type.DESSERT;
     }
 
     public boolean isMain() {
-        return this == T_BONE_STEAK || this == BARBECUE_RIBS || this == SEAFOOD_PASTA || this == CHRISTMAS_PASTA;
+        return this.type == Type.MAIN;
     }
 
     public boolean isNotBeverage() {
-        return this != ZERO_COKE && this != RED_WINE && this != CHAMPAGNE;
+        return this.type != Type.BEVERAGE;
     }
 
     public String getName() {
         return name;
+    }
+
+    private enum Type {
+        APPETIZER, MAIN, DESSERT, BEVERAGE
     }
 }
