164900670b07b2c1e97a263135ef871da1925b11
hlmg <devhlmg@gmail.com>
2023-11-15 06:26:40 +0900

    fix(InputView): 메뉴 문자열 끝에 "," 예외처리 안되는 문제 수정
    수정된 정규식 주의점
    - 그룹을 캡처할 필요가 없어서 Non-capturing group을 사용한다. (?:)
    - 백트래킹을 사용하지 않기 위해 Possessive quantifiers를 사용한다 *+

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -8,13 +8,12 @@ import christmas.dto.OrderMenuDto;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 public final class InputView {
     private static final String INPUT_VISIT_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
     private static final String INPUT_ORDER_MENU = "주문하실 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
     private static final Pattern VISIT_DATE_PATTERN = Pattern.compile("^([1-9]|[12]\\d|3[01])$");
-    private static final Pattern ORDER_MENU_PATTERN = Pattern.compile("^([가-힣a-zA-Z]+-[0-9]+,?)+$");
+    private static final Pattern ORDER_MENU_PATTERN = Pattern.compile("^(?:[가-힣a-zA-Z]+-\\d+,)*+[가-힣a-zA-Z]+-\\d+$");
     private static final String ORDER_MENU_SEPARATOR = ",";
     private static final String MENU_QUANTITY_SEPARATOR = "-";
 
@@ -48,6 +47,6 @@ public final class InputView {
         return Arrays.stream(orderMenuInput.split(ORDER_MENU_SEPARATOR))
                 .map(orderMenu -> orderMenu.split(MENU_QUANTITY_SEPARATOR))
                 .map(menuAndQuantity -> new OrderMenuDto(menuAndQuantity[0], Integer.parseInt(menuAndQuantity[1])))
-                .collect(Collectors.toList());
+                .toList();
     }
 }
